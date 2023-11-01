4610a55c8f81523030b97f15491e2a54d4e4d397
charles017 <charlie.abuloc@myyahoo.com>
2023-11-01 14:57:07 +0800

    Initial edit

---
+++ main.js
@@ -0,0 +1,20 @@
+async function start() {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all");
+  const data = await response.json;
+  createBreedList(data.message);
+}
+
+start();
+
+function createBreedList(breedList) {
+  document.getElementById("breed").innerHTML = `
+    <select class="form-select form-select-lg mb-3" aria-label="Large select example">
+            <option selected class="text-center">Choose a dog breed</option>
+            ${Object.keys(breedList)
+              .map(function (breed) {
+                return `<option>${breed}</option>`;
+              })
+              .join("")}
+          </select>
+    `;
+}
