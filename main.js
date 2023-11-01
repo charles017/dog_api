5756f81f0a1321aaa18ec294f1bd4adcb7342a6e
charles017 <charlie.abuloc@myyahoo.com>
2023-11-01 15:10:29 +0800

    drafts

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 async function start() {
-  const response = await fetch("https://dog.ceo/api/breeds/list/all");
-  const data = await response.json;
-  createBreedList(data.message);
+  const response = await fetch("https://dog.ceo/api/breeds/list/all")
+  const data = await response.json
+  createBreedList(data.message)
 }
 
 start()
